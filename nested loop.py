1a4ef06e975bb0ddf8feee8eb3a580b71c5107a1
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-04-08 19:30:02 +0530

    Add files via upload

---
+++ nested loop.py
@@ -0,0 +1,7 @@
+#Anested loop is a loop inside a loop.
+#The "inner loop" will be
+adj=["red","big","tasty"]
+fruits=["Apple","Banana","cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
